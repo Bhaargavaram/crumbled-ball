9a65e6675255be0b7b8384f9ce5c2a181c76a840
Bhaargavaram <76215644+Bhaargavaram@users.noreply.github.com>
2021-03-02 10:09:12 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -44,7 +44,7 @@ function keyPressed(){
 
 	if(keyCode === UP_ARROW) {
 
-		Matter.Body.applyForce(paper1.body,paper1.body.position,{x:130,y:-145});
+		Matter.Body.applyForce(paper1.body,paper1.body.position,{x:130,y:140});
 	}
   
 }
