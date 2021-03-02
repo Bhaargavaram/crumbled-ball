d643b291ae94fecf3fc213b7a4613c972777d98c
Bhaargavaram <76215644+Bhaargavaram@users.noreply.github.com>
2021-03-02 10:02:19 +0530

    Update paper.js

--- paper.js
+++ paper.js
@@ -10,13 +10,14 @@ class Paper {
             density : 1.2			
 			
 		}
+		this.x = x;
+		this.y = y;
+		this.r=r;
 
 		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options) 
         World.add(world, this.body);
 
-		this.x = x;
-		this.y = y;
-		this.r=r;
+		
 
 		//this.width = 77;
 		//this.height = 77;
